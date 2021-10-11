4e89257a04d95d7aa2cede0f2b51cc1e5286056d
junchoi0408 <junchoi0408@gmail.com>
2021-10-11 13:49:44 -0400

    initalized and created necessary components for the project

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Cloud, Github, LinkedIn, Instagram } from '@mui/icons-material';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1><Cloud fontSize="large"/>JC</h1>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
