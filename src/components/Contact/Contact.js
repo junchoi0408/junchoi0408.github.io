37da7313406665bb06aa49352e944febcae35592
junchoi0408 <junchoi0408@gmail.com>
2021-10-13 12:44:37 -0400

    updated the h3 element to h2

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
                 <div className="hr__container" id="experience">
                     <hr className="gradient__hr"/>
                 </div>
-                <h3 className="title">Get In Touch</h3>
+                <h2 className="title">Get In Touch</h2>
                 <p className="about__description">I'm currently looking for new opportunities to build exciting applications with great people! If you have any questions or just want to say hi, please feel free to reach out to me. I'll try my best to get back as soon as possible.</p>
             </div>
         </>
