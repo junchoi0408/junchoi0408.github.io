8f8f50f135c8bba36ac4444147ca626963ffbfd7
junchoi0408 <junchoi0408@gmail.com>
2021-10-15 11:52:21 -0400

    added webkit and gradient

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -71,7 +71,7 @@ const Experience = () => {
                         </Tabs>
                     </Box>
                     <div className="job__container .container">
-                        <h3 style={{marginTop: 0, fontWeight: 500, marginBottom: 0}}>{title} <span className="company__link">@<a href={link}>{company}</a></span></h3>
+                        <h3 className="gradient-text" style={{marginTop: 0, fontWeight: 500, marginBottom: 0}}>{title} <span className="company__link">@<a href={link}>{company}</a></span></h3>
                         <p style={{marginTop: '0.5em', color: '#979DAC', fontSize: '14px', marginBottom: "2em"}}>{dates}</p>
                         {duties.map((duty, index)=>
                         {
