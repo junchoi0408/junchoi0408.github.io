5ae48c7b575397984a7a55fe81ac91b00edf3fe6
junchoi0408 <junchoi0408@gmail.com>
2021-10-14 21:34:58 -0400

    added eruda for inspecting on mobile ios

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -21,39 +21,38 @@ const Experience = () => {
                 <h2 className="title">Experience</h2>
                 <h4 className="about__description" style={{marginBottom: '1em', color: "#7D8597"}}>My mission is to pursue lifelong learning and growth and to share my experience and knowledge with others.</h4>
                 <div className="job__small">
-                    
-                <Box style={{ marginBottom: '3em' }}>
-                    <Tabs
-                        value={id}
-                        onChange={handleChange}
-                        variant="scrollable"
-                        scrollButtons={false}
-                        textColor="inherit"
-                        indicatorColor="inherit"
-                        aria-label="list of companies"
-                    >
-                        {jobs.map(({company, id})=>{
-                            return (
-                                <Tab disableRipple key={id} label={company} />
-                            )
-                        })}
-                    </Tabs>
-                </Box>
-                <div className="job__container .container">
-                    <h3 style={{marginTop: 0, fontWeight: 500, marginBottom: 0}}>{title} <span className="company__link">@<a href={link}>{company}</a></span></h3>
-                    <p style={{marginTop: '0.5em', color: '#979DAC', fontSize: '14px', marginBottom: "2em"}}>{dates}</p>
-                    {duties.map((duty, index)=>
-                    {
-                        return(
-                            <div className="job-desc" key={index}>
-                                <TiChevronRight className="right" style={{color:"#92b3ec"}}/>
-                                <p className="job" style={{color: '#7D8597', lineHeight: "1.45", fontSize: '15px'}}>{duty}</p>
-                            </div>
-                    )})}
-                </div>
+                    <Box style={{ marginBottom: '2.5em' }}>
+                        <Tabs
+                            value={id}
+                            onChange={handleChange}
+                            variant="scrollable"
+                            scrollButtons={false}
+                            textColor="inherit"
+                            indicatorColor="inherit"
+                            aria-label="list of companies"
+                        >
+                            {jobs.map(({company, id})=>{
+                                return (
+                                    <Tab disableRipple key={id} label={company} />
+                                )
+                            })}
+                        </Tabs>
+                    </Box>
+                    <div className="job__container .container">
+                        <h3 style={{marginTop: 0, fontWeight: 500, marginBottom: 0}}>{title} <span className="company__link">@<a href={link}>{company}</a></span></h3>
+                        <p style={{marginTop: '0.5em', color: '#979DAC', fontSize: '14px', marginBottom: "2em"}}>{dates}</p>
+                        {duties.map((duty, index)=>
+                        {
+                            return(
+                                <div className="job-desc" key={index}>
+                                    <TiChevronRight className="right" style={{color:"#92b3ec"}}/>
+                                    <p className="job" style={{color: '#7D8597', lineHeight: "1.45", fontSize: '15px'}}>{duty}</p>
+                                </div>
+                        )})}
+                    </div>
                 </div>
                 <div className="job__big">
-                    <Box style={{ marginBottom: '3em' }}>
+                    <Box style={{ marginBottom: '2.5em' }}>
                         <Tabs
                             value={id}
                             onChange={handleChange}
